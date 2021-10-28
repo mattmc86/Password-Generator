ad7d07c3fc420461caa9ac6bcafde4dc73224cd4
Matt <86723332+mattmc86@users.noreply.github.com>
2021-10-28 15:41:49 +0100

    Assigned variables for each user option

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ function writePassword() {
 
   passwordText.value = password;
 
+  let passwordLength = prompt('How many characters do you want the password to be - min 8 max 128');
+  let passwordSpecial = window.confirm("Click OK if you want to include special characters");
+  let passwordNumeric = window.confirm("Click OK if you want to include numeric characters");
+  let passwordLower = window.confirm("Click OK if you want to include lowercase characters");
+  let passwordUpper = window.confirm("Click OK if you want to include uppercase characters");
+    console.log(passwordLength)
+    console.log(passwordSpecial)
+    console.log(passwordNumeric)
+    console.log(passwordLower)
+    console.log(passwordUpper)
+
+
 }
 
 // Add event listener to generate button
